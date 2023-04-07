bc0495f85fe056a1ace7417f1e5de98977bb75f3
MarkoErdelji <erdeljimarko@gmail.com>
2023-04-07 18:39:30 +0200

    feat: Added map highlighting when clicking on country

--- src/Services/CountriesService.ts
+++ src/Services/CountriesService.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import osmtogeojson from "osmtogeojson";
 
 export const getCountries = () =>
 {
@@ -21,3 +22,20 @@ export const getCountriesByName = (name:string) =>
 }
 
 
+export const getGeoJSON = (relationURL:string) => {
+    const relationID = relationURL.match(/\/relation\/(\d+)/)![1];
+    const apiUrl = `https://overpass-api.de/api/interpreter?data=[out:json];relation(${relationID});out geom;`;
+    
+    let response = axios.get(apiUrl);
+  
+    return axios.get(apiUrl)
+    .then((response) => {
+      const elements = response.data;
+      const geojson = osmtogeojson(elements);
+      return geojson;
+    })
+    .catch((error) => {
+      console.error(error);
+      return null;
+    });
+  }
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,46 +1,67 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import './Map.css';
 import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 import "leaflet/dist/leaflet.css";
-import {LatLngExpression} from "leaflet";
+import { LatLngExpression, Point } from "leaflet";
+import { getGeoJSON } from '../../Services/CountriesService';
+import L from 'leaflet';
 
 interface MapProps {
-    selectedCountry: string;
+  selectedCountry: any;
 }
 
 const Map: FC<MapProps> = ({ selectedCountry }) => {
-    console.log(selectedCountry)
-    return(
-        <MapContainer center={[20, 0]} zoom={3} scrollWheelZoom={false} dragging={false} zoomControl={false} doubleClickZoom={false} >
-            <MapInner selectedCountry={selectedCountry} />
-        </MapContainer>
-    );
+  console.log(selectedCountry)
+  return (
+    <MapContainer center={[20, 0]} zoom={3} scrollWheelZoom={false} dragging={false} zoomControl={false} doubleClickZoom={false} >
+      <MapInner selectedCountry={selectedCountry} />
+    </MapContainer>
+  );
 }
 
 
 interface MapInnerProps {
-    selectedCountry: string;
+  selectedCountry: any;
 }
 
 const MapInner: FC<MapInnerProps> = ({ selectedCountry }) => {
-    const map = useMap()
-    if (selectedCountry != '') {
-        // @ts-ignore
-        console.log(selectedCountry)
-        // @ts-ignore
+  const map = useMap()
 
-        map.setView([selectedCountry[0], selectedCountry[1]],6)
+  useEffect(() => {
+    if (selectedCountry !== '') {
+      getGeoJSON(selectedCountry.maps.openStreetMaps).then((geojson) => {
+        if (geojson !== null) {
+            map.eachLayer(layer => {
+                if (!(layer instanceof L.TileLayer)) {
+                  map.removeLayer(layer);
+                }
+              });
+            // add the highlight layer for the country
+            L.geoJSON(geojson, {
+            style: {
+                weight: 3,
+                color: 'blue',
+                fillOpacity: 0.1
+            }
+            }).addTo(map);
+        
+            // fit the map to the bounds of the country
+            const bounds = L.geoJSON(geojson).getBounds();
+            map.fitBounds(bounds);
+        }
+      });
     }
+  }, [selectedCountry, map]);
 
-    return(
-        <>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {/* Add any additional map elements here */}
-        </>
-    );
+  return (
+    <>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* Add any additional map elements here */}
+    </>
+  );
 }
 
 export default Map;
